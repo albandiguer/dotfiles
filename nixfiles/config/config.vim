b93cea72bed690c97e93f5d75e0bbf340e252298
Alban Diguer <alban.diguer@gmail.com>
2022-07-03 13:01:16 +0200

    Add Trouble and Null LS

--- nixfiles/config/config.vim
+++ nixfiles/config/config.vim
@@ -30,8 +30,10 @@ map <C-a> <esc>ggVG<CR>
 xnoremap p pgvy
 
 " easy lopen lclose
-nmap ( :lopen<CR>
-nmap ) :lclose<CR>
+" nmap ( :lopen<CR>
+" nmap ) :lclose<CR>
+nmap ( :Trouble<CR>
+nmap ) :TroubleClose<CR>
 
 " when a bracket is inserted, briefly show the matching one
 set showmatch
@@ -84,13 +86,7 @@ nnoremap K :Ack! "\b<cword>\b" <CR>
 lua << EOF
 
 
-	local lspconfig = require('lspconfig')
 	local cmp = require'cmp'
-	-- run :LspInstall <lsp> in vim to install servers
-
-	-- previously we were doing, no lazy loading
-	-- lspconfig.tsserver.setup{on_attach=require'completion'.on_attach}
-
 
 	require'nvim-treesitter.configs'.setup {
 		-- Modules and its options go here
@@ -143,31 +139,118 @@ lua << EOF
 	-- Register a handler that will be called for all installed servers.
 	-- Alternatively, you may also register handlers on specific server instances instead (see example below).
 
+	local opts = { noremap=true, silent=true }
+	vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+	vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+	vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+	vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+	-- Use an on_attach function to only map the following keys
+	-- after the language server attaches to the current buffer
+	local on_attach = function(client, bufnr)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+		-- Mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local bufopts = { noremap=true, silent=true, buffer=bufnr }
+		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+		vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+		vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+		vim.keymap.set('n', '<space>wl', function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, bufopts)
+		vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+		vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+		vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+		vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+		vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+
+		-- VISIT https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save#choosing-a-client-for-formatting
+		if client.name == "tsserver" then
+			-- let null-ls do the formatting
+			client.resolved_capabilities.document_formatting = false
+		end
+	end
+
+
+	-- The only important thing is to make sure to call require("nvim-lsp-installer").setup {} before interacting with lspconfig
+	require("nvim-lsp-installer").setup({
+		automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+	})
+
+	local lspconfig = require('lspconfig')
+
 	-- Register a handler for the "javascript" language.
 	lspconfig.tsserver.setup {
 		-- See documentation for the options.
-		capabilities = capabilities
+		capabilities = capabilities,
+		on_attach = on_attach
+
 	}
 
-	require("nvim-lsp-installer").setup({
-		capabilities = capabilities,
-		automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
-		ui = {
-			icons = {
-				server_installed = "✓",
-				server_pending = "➜",
-				server_uninstalled = "✗"
-			}
-		}
-	})
+	-- Async formatting on save callback
+	-- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save#code-1
+	local async_formatting = function(bufnr)
+	    bufnr = bufnr or vim.api.nvim_get_current_buf()
 
-	require("null-ls").setup({
+	    vim.lsp.buf_request(
+		bufnr,
+		"textDocument/formatting",
+		{ textDocument = { uri = vim.uri_from_bufnr(bufnr) } },
+		function(err, res, ctx)
+		    if err then
+			local err_msg = type(err) == "string" and err or err.message
+			-- you can modify the log message / level (or ignore it completely)
+			vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+			return
+		    end
+
+		    -- don't apply results if buffer is unloaded or has been modified
+		    if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+			return
+		    end
+
+		    if res then
+			local client = vim.lsp.get_client_by_id(ctx.client_id)
+			vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
+			vim.api.nvim_buf_call(bufnr, function()
+			    vim.cmd("silent noautocmd update")
+			end)
+		    end
+		end
+	    )
+	end
+
+
+	local null_ls = require("null-ls")
+	null_ls.setup({
 	    sources = {
-		require("null-ls").builtins.formatting.stylua,
-		require("null-ls").builtins.formatting.prettier,
-		require("null-ls").builtins.diagnostics.eslint,
-		require("null-ls").builtins.completion.spell
+		null_ls.builtins.code_actions.statix,
+		null_ls.builtins.completion.spell,
+		null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.diagnostics.vint,
+		null_ls.builtins.formatting.alejandra,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.stylua,
 	    },
+		debug = false,
+		on_attach = function(client, bufnr)
+			if client.supports_method("textDocument/formatting") then
+			    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			    vim.api.nvim_create_autocmd("BufWritePost", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+				    async_formatting(bufnr)
+				end,
+			    })
+			end
+		    end
 	})
 
 	require("trouble").setup({
--- nixfiles/config/neovim.nix
+++ nixfiles/config/neovim.nix
@@ -1,5 +1,8 @@
-{ config, pkgs, ... }: {
-
+{
+  config,
+  pkgs,
+  ...
+}: {
   # For black (python fixer) we allow broken
   nixpkgs.config.allowBroken = true;
 
@@ -11,20 +14,20 @@
     # Enable Python 3 provider.
     # https://rycee.gitlab.io/home-manager/options.html#opt-programs.neovim.withPython3
     withPython3 = true; # default is true so this redundant
-    extraPython3Packages = (ps:
+    extraPython3Packages = ps:
       with ps; [
         black # this one currently broken because of https://github.com/NixOS/nixpkgs/pull/172397
         flake8
-      ]);
+      ];
 
     # withNodeJs = true; no extraNodePackages to do the same as above with python?
 
     # Extra packages available to nvim
     # https://rycee.gitlab.io/home-manager/options.html#opt-programs.neovim.extraPackages
     extraPackages = with pkgs; [
-      deno
-      nixfmt
-      rustfmt
+      # deno
+      # nixfmt
+      # rustfmt
       wget # used by lsp-installer , use :checkhealth in vim
       # texlive.combined.scheme-full
 
@@ -39,96 +42,100 @@
       # nodePackages.lehre
       # nodePackages.typescript
       # nodePackages.typescript-language-server
-      # nodePackages.prettier
-      # nodePackages.eslint
+
+      # null-ls deps
+      alejandra
+      statix
+      nodePackages.prettier
+      nodePackages.eslint
+      vim-vint
 
       # elmPackages.elm-format
       # rust-analyzer
       # haskellPackages.brittany
     ];
 
-    plugins = with pkgs.vimPlugins;
-      let
-        # plugin that shows the context - top of the screen above horizontal bar
-        context-vim = pkgs.vimUtils.buildVimPlugin {
-          name = "context-vim";
-          src = pkgs.fetchFromGitHub {
-            owner = "wellle";
-            repo = "context.vim";
-            rev = "e38496f1eb5bb52b1022e5c1f694e9be61c3714c";
-            sha256 = "1iy614py9qz4rwk9p4pr1ci0m1lvxil0xiv3ymqzhqrw5l55n346";
-          };
+    plugins = with pkgs.vimPlugins; let
+      # plugin that shows the context - top of the screen above horizontal bar
+      context-vim = pkgs.vimUtils.buildVimPlugin {
+        name = "context-vim";
+        src = pkgs.fetchFromGitHub {
+          owner = "wellle";
+          repo = "context.vim";
+          rev = "e38496f1eb5bb52b1022e5c1f694e9be61c3714c";
+          sha256 = "1iy614py9qz4rwk9p4pr1ci0m1lvxil0xiv3ymqzhqrw5l55n346";
         };
+      };
 
-        catppuccin-vim = pkgs.vimUtils.buildVimPlugin {
-          name = "catppuccin-vim";
-          src = pkgs.fetchFromGitHub {
-            owner = "catppuccin";
-            repo = "nvim";
-            rev = "22b34eb9f93430bc010dee1523743b62cd2700fc";
-            sha256 = "1cqc6pws6czdshzxmh89ryf2j1cc2n5maf8v5v6nh8yw02jqghk5";
-            fetchSubmodules = true;
-          };
+      catppuccin-vim = pkgs.vimUtils.buildVimPlugin {
+        name = "catppuccin-vim";
+        src = pkgs.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "nvim";
+          rev = "22b34eb9f93430bc010dee1523743b62cd2700fc";
+          sha256 = "1cqc6pws6czdshzxmh89ryf2j1cc2n5maf8v5v6nh8yw02jqghk5";
+          fetchSubmodules = true;
         };
+      };
 
-        # NOTE cannot update this one at the moment
-        # hence dontBuild dontCheck
-        nvim-lsp-installer = pkgs.vimUtils.buildVimPlugin {
-          name = "nvim-lsp-installer";
-          dontBuild = true;
-          dontCheck = true;
-          src = pkgs.fetchFromGitHub {
-            owner = "williamboman";
-            repo = "nvim-lsp-installer";
-            rev = "c13ea61d85e2170af35c06b47bcba143cf2f244b";
-            sha256 = "0p2x098z9cl5mz095f1nr7714bmxpcvyrfhwh5hrrggl1dhqflrh";
-            fetchSubmodules = false;
-          };
+      # NOTE cannot update this one at the moment
+      # hence dontBuild dontCheck
+      nvim-lsp-installer = pkgs.vimUtils.buildVimPlugin {
+        name = "nvim-lsp-installer";
+        dontBuild = true;
+        dontCheck = true;
+        src = pkgs.fetchFromGitHub {
+          owner = "williamboman";
+          repo = "nvim-lsp-installer";
+          rev = "c13ea61d85e2170af35c06b47bcba143cf2f244b";
+          sha256 = "0p2x098z9cl5mz095f1nr7714bmxpcvyrfhwh5hrrggl1dhqflrh";
+          fetchSubmodules = false;
         };
+      };
+    in [
+      ack-vim
+      ayu-vim
+      catppuccin-vim
+      cmp-buffer
+      cmp-cmdline
+      cmp-copilot
+      cmp-nvim-lsp
+      cmp-path
+      cmp-vsnip
+      context-vim
+      copilot-vim # github copilot
+      delimitMate
+      editorconfig-vim
+      fzf-vim # Replace with Telescope
+      nerdtree
+      null-ls-nvim
+      nvim-cmp
+      nvim-lsp-installer
+      nvim-lspconfig
+      nvim-treesitter
+      nvim-web-devicons
+      tabular
+      trouble-nvim
+      vim-airline
+      vim-better-whitespace
+      vim-bookmarks
+      vim-commentary
+      vim-devicons
+      vim-dispatch
+      vim-fugitive
+      vim-gist
+      vim-jsdoc
+      vim-nix # so it is not lost with nix file, commenting etc
+      vim-obsession
+      vim-prettier
+      vim-slash
+      vim-tmux-navigator # seamless ctrl-hjkl navigation with tmux
+      vim-vsnip
+      webapi-vim # used by vim-gist for api call
 
-      in [
-        ack-vim
-        ayu-vim
-        catppuccin-vim
-        cmp-buffer
-        cmp-cmdline
-        cmp-copilot
-        cmp-nvim-lsp
-        cmp-path
-        cmp-vsnip
-        context-vim
-        copilot-vim # github copilot
-        delimitMate
-        editorconfig-vim
-        fzf-vim # Replace with Telescope
-        nerdtree
-        null-ls-nvim
-        nvim-cmp
-        nvim-lsp-installer
-        nvim-lspconfig
-        nvim-treesitter
-        tabular
-        trouble-nvim
-        vim-airline
-        vim-better-whitespace
-        vim-bookmarks
-        vim-commentary
-        vim-devicons
-        vim-dispatch
-        vim-fugitive
-        vim-gist
-        vim-jsdoc
-        vim-nix # so it is not lost with nix file, commenting etc
-        vim-obsession
-        vim-prettier
-        vim-slash
-        vim-tmux-navigator # seamless ctrl-hjkl navigation with tmux
-        webapi-vim # used by vim-gist for api call
-
-        # or you can use our function to directly fetch plugins from git
-        # (plugin "schickling/vim-bufonly")
-
-      ]; # Only loaded if programs.neovim.extraConfig is set
+      # or you can use our function to directly fetch plugins from git
+      # (plugin "schickling/vim-bufonly")
+    ]; # Only loaded if programs.neovim.extraConfig is set
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
