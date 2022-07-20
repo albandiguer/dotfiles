09df05336ddd3de3e946703f26c760145959b82f
Alban Diguer <alban.diguer@gmail.com>
2022-07-20 17:15:50 +0200

    Checkin files post tf upgrade to 0.13

--- aws-boilerplate-tf/shell.nix
+++ aws-boilerplate-tf/shell.nix
@@ -1,4 +1,6 @@
 let
+  # List of available packages/versions here
+  # https://lazamar.co.uk/nix-versions/?package=terraform&version=0.13.7&fullName=terraform-0.13.7&keyName=terraform_0_13&revision=bf972dc380f36a3bf83db052380e55f0eaa7dcb6&channel=nixpkgs-unstable#instructions
   pkgs = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/bf972dc380f36a3bf83db052380e55f0eaa7dcb6.tar.gz";
   }) {};
