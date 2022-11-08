e723ab6c856440ae79e26f0efb214d39661e68e8
Alban Diguer <alban.diguer@pretto.fr>
2022-11-08 08:42:49 +0100

    feat: nvim-tree adaptive size

--- nixfiles/nvim/lua/nvim-tree.lua
+++ nixfiles/nvim/lua/nvim-tree.lua
@@ -1,7 +1,11 @@
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+	view = {
+		adaptive_size = true;
+	}
+})
 
 map('n', '<C-n>', ':NvimTreeToggle')
 map('n', '<C-b>', ':NvimTreeFindFile<CR>')
