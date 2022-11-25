43c7ced2a0dd381747f39ce8ab9ad6b9613cea25
Alban Diguer <alban.diguer@gmail.com>
2022-11-25 20:48:51 +0100

    starship config

---
+++ nixfiles/starship.nix
@@ -0,0 +1,19 @@
+{ config
+, pkgs
+, lib
+, ...
+}: {
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = true;
+
+      # character = {
+      #   success_symbol = "[➜](bold green)";
+      #   error_symbol = "[➜](bold red)";
+      # };
+
+      # package.disabled = true;
+    };
+  };
+}
