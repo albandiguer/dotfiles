faa814a60e39af2b8e4219352a23bc929590c58e
Alban Diguer <alban.diguer@gmail.com>
2024-02-04 02:15:18 +0100

    feat: add html snippets in eruby

--- nixfiles/programs/nvim/lua/snippets.lua
+++ nixfiles/programs/nvim/lua/snippets.lua
@@ -1,5 +1,8 @@
 local luasnip = require('luasnip')
+-- Extend filetypes for ERB to include "html"
 luasnip.filetype_extend("ruby", { "rails" })
+-- Extend filetypes for ERB to include "html"
+luasnip.filetype_extend("eruby", { "html" })
 -- below loads friendly-snippets see doc here
 -- https://github.com/L3MON4D3/LuaSnip#add-snippets
 require("luasnip.loaders.from_vscode").lazy_load()
