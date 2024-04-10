0c6086350dc6624481eb3e6d0f801f751754b2ab
Alban Diguer <alban.diguer@gmail.com>
2024-04-10 23:01:08 +0200

    fix: broken obsidian mappings

--- previously/programs/nvim/lua/obsidian.lua
+++ previously/programs/nvim/lua/obsidian.lua
@@ -20,53 +20,38 @@ require("obsidian").setup({
 	},
 	-- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
 	completion = {
-		-- Set to false to disable completion.
 		nvim_cmp = true,
 		-- Trigger completion at 2 chars.
 		min_chars = 2,
 	},
-	mappings = {
-		-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-		["gf"] = {
-			action = function()
-				return require("obsidian").util.gf_passthrough()
-			end,
-			opts = { noremap = false, expr = true, buffer = true },
-		},
-		-- Toggle check-boxes.
-		-- ["<c-x>"] = {
-		-- 	action = function()
-		-- 		return require("obsidian").util.toggle_checkbox()
-		-- 	end,
-		-- 	opts = { buffer = true },
-		-- },
-		-- Smart action depending on context, either follow link or toggle checkbox.
-		["<cr>"] = {
-			action = function()
-				return require("obsidian").util.smart_action()
-			end,
-			opts = { buffer = true },
-		},
-		-- Open the daily note for the current date.
-		-- ["<c-t>"] = {
-		-- 	action = function()
-		-- 		return require("obsidian").util.open_daily_note()
-		-- 	end,
-		-- 	opts = { buffer = true },
-		-- },
-		-- Open note in obsidian
-		-- ["<c-o>"] = {
-		-- 	action = function()
-		-- 		return require("obsidian").util.open_note()
-		-- 	end,
-		-- 	opts = { buffer = true },
-		-- },
-		-- Search in obsidian
-		-- ["<c-p>"] = {
-		-- 	action = function()
-		-- 		return require("obsidian").util.search()
-		-- 	end,
-		-- 	opts = { buffer = true },
-		-- },
-	}
+	-- mappings = {
+	-- Toggle check-boxes.
+	-- ["<c-x>"] = {
+	-- 	action = function()
+	-- 		return require("obsidian").util.toggle_checkbox()
+	-- 	end,
+	-- 	opts = { buffer = true },
+	-- },
+	-- Open the daily note for the current date.
+	-- ["<c-t>"] = {
+	-- 	action = function()
+	-- 		return require("obsidian").util.open_daily_note()
+	-- 	end,
+	-- 	opts = { buffer = true },
+	-- },
+	-- Open note in obsidian
+	-- ["<c-o>"] = {
+	-- 	action = function()
+	-- 		return require("obsidian").util.open_note()
+	-- 	end,
+	-- 	opts = { buffer = true },
+	-- },
+	-- Search in obsidian
+	-- ["<c-p>"] = {
+	-- 	action = function()
+	-- 		return require("obsidian").util.search()
+	-- 	end,
+	-- 	opts = { buffer = true },
+	-- },
+	-- }
 })
