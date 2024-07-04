3976270f2116a58403a26201a10bfadb3c157577
Alban Diguer <alban.diguer@gmail.com>
2024-07-04 02:07:37 +0200

    feat: +alacritty

--- previously/programs/home-manager.nix
+++ previously/programs/home-manager.nix
@@ -6,4 +6,16 @@
 }: {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  # TODO: extract
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      # Change font to monaco nerd font
+      font = {
+        family = "Monaco Nerd Font Mono";
+        size = 15;
+      };
+    };
+  };
 }
