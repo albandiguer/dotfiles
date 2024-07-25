ff50bfdceed82f7f1bbad0dc12f3e80515425677
Alban Diguer <alban.diguer@gmail.com>
2024-07-26 01:31:31 +0200

    chore: config

--- home/programs/kickstart-nvim/nvim/lua/kickstart/plugins/neo-tree.lua
+++ home/programs/kickstart-nvim/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -2,24 +2,29 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-  },
+	'nvim-neo-tree/neo-tree.nvim',
+	version = '*',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+		'MunifTanjim/nui.nvim',
+	},
+	cmd = 'Neotree',
+	keys = {
+		{ '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
+	},
+	opts = {
+		filesystem = {
+			filtered_items = {
+				hide_hidden = false,
+				hide_dotfiles = false,
+				hide_gitignored = false,
+			},
+			window = {
+				mappings = {
+					['\\'] = 'close_window',
+				},
+			},
+		},
+	},
 }
