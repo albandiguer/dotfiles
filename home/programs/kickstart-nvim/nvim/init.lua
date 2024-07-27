797e8bb469f65ab9a76de22b200bea6163cd4fe0
Alban Diguer <alban.diguer@gmail.com>
2024-07-28 00:09:26 +0200

    feat: c-space for text objects selection

--- home/programs/kickstart-nvim/nvim/init.lua
+++ home/programs/kickstart-nvim/nvim/init.lua
@@ -878,6 +878,16 @@ require('lazy').setup({
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+      textobjects = { enable = true },
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
