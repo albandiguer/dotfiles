af2332d8bd02c4141f0d1ff6cafdbe68807af9f8
Alban Diguer <alban.diguer@gmail.com>
2024-07-30 20:26:38 +0200

    fix: snippets

--- home/programs/kickstart-nvim/nvim/init.lua
+++ home/programs/kickstart-nvim/nvim/init.lua
@@ -684,33 +684,29 @@ require('lazy').setup({
     dependencies = {
       -- Snippet Engine & its associated nvim-cmp source
       {
-        'saadparwaiz1/cmp_luasnip',
+        'L3MON4D3/LuaSnip',
+        lazy = false,
+        -- build = (function() -- NOTE: not working?
+        --   -- Build Step is needed for regex support in snippets.
+        --   -- This step is not supported in many windows environments.
+        --   -- Remove the below condition to re-enable on windows.
+        --   if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+        --     return
+        --   end
+        --   return 'make install_jsregexp'
+        -- end)(),
         dependencies = {
-          'L3MON4D3/LuaSnip',
-          lazy = false,
-          build = (function()
-            -- Build Step is needed for regex support in snippets.
-            -- This step is not supported in many windows environments.
-            -- Remove the below condition to re-enable on windows.
-            if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-              return
-            end
-            return 'make install_jsregexp'
-          end)(),
-          dependencies = {
-            -- `friendly-snippets` contains a variety of premade snippets.
-            --    See the README about individual language/framework/plugin snippets:
-            --    https://github.com/rafamadriz/friendly-snippets
-            {
-              'rafamadriz/friendly-snippets',
-              config = function()
-                require('luasnip.loaders.from_vscode').lazy_load()
-              end,
-            },
+          -- `friendly-snippets` contains a variety of premade snippets.
+          --    See the README about individual language/framework/plugin snippets:
+          --    https://github.com/rafamadriz/friendly-snippets
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+              --   require('luasnip.loaders.from_lua').load { paths = '~/.snippets' }
+            end,
           },
-          -- config = function()
-          --   require('luasnip.loaders.from_lua').load { paths = '~/.snippets' }
-          -- end,
+          'saadparwaiz1/cmp_luasnip',
         },
       },
 
