5eb14a8dec73126b9bd29cc85dd4eeb6c833cde2
Alban Diguer <alban.diguer@pretto.fr>
2024-09-17 14:27:01 +0200

    chore: +ensure installed mason tools

--- home/programs/neovim/nvim/init.lua
+++ home/programs/neovim/nvim/init.lua
@@ -638,8 +638,17 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
+        'bashls',
+        'dockerls',
+        'hadolint',
+        'lua_ls',
+        'nil_ls', -- nix lsp
         'stylua', -- Used to format Lua code
-        'nil_ls'  -- nix lsp
+        'terraformls',
+        'tflint',
+        'ts_ls',
+        'yamlfmt',
+        'yamlls',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
