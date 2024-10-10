1df23166b6ce71d6115fd62d6100d404aef10312
Alban Diguer <alban.diguer@pretto.fr>
2024-10-11 00:12:22 +0200

    feat: +alogs

--- home/programs/fish/default.nix
+++ home/programs/fish/default.nix
@@ -32,7 +32,27 @@
       tf = "terraform";
     };
 
-    # functions = {};
+    functions = {
+      alogs = ''
+        function alogs
+          echo "Enter AWS profile (default: pretto-prod):"
+          read -l aws_profile
+          if not set -q aws_profile
+            set aws_profile pretto-prod
+          end
+          echo "Verifying AWS SSO session..."
+          if not aws sso list-accounts --profile $aws_profile --config ~/.aws/config > /dev/null 2>&1
+            aws sso login --profile $aws_profile
+          end
+          echo "Select log group..."
+          set selected_group (awslogs groups --profile pretto-prod | fzf)
+          echo "Add any filters you like..."
+          read -l filters
+          echo "Fetching logs..."
+          awslogs get $selected_group --timestamp --profile pretto-prod $filters
+        end
+      '';
+    };
 
     plugins = [
       # BUG makes `vim` cmd work somehow, nvim is borked to old version, not getting why
