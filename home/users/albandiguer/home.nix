247cb1d855e118b4d0cd30617eb8f2b329b2e4b9
Alban Diguer <alban.diguer@pretto.fr>
2024-10-18 10:53:54 +0200

    chore: +ghstackrc

--- home/users/albandiguer/home.nix
+++ home/users/albandiguer/home.nix
@@ -63,6 +63,7 @@
       # "Google Drive".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Library/CloudStorage/GoogleDrive-alban.diguer@gmail.com/My Drive";
       ".default-gems".source = ../../dotfiles/.default-gems; # TODO: move in mise.nix
       ".default-python-packages".source = ../../dotfiles/.default-python-packages;
+      ".ghstackrc".source = ../../dotfiles/.ghstackrc;
     };
 
     # Home Manager needs a bit of information about you and the
