ec4346719ea7351ed57087cffc2b39931f186000
Alban Diguer <alban.diguer@pretto.fr>
2024-10-18 10:24:29 +0200

    chore: -skhd -yabai configs

--- darwin/macbook.nix
+++ darwin/macbook.nix
@@ -15,83 +15,6 @@
     # Auto upgrade nix package and the daemon service.
     nix-daemon.enable = true;
 
-    yabai = {
-      enable = false;
-      config = {
-        mouse_follows_focus = "off";
-        focus_follows_mouse = "off";
-        window_placement = "second_child";
-        window_topmost = "off";
-        window_opacity = "off";
-        window_opacity_duration = "0.0";
-        window_shadow = "off";
-        window_border = "on";
-        window_border_width = 1;
-        active_window_border_color = "0xFF40FF00";
-        normal_window_border_color = "0x00FFFFFF";
-        active_window_opacity = 1.0;
-        normal_window_opacity = 0.90;
-        split_ratio = 0.50;
-        auto_balance = "off";
-        mouse_modifier = "fn";
-        mouse_action1 = "move";
-        mouse_action2 = "resize";
-        layout = "bsp";
-        top_padding = 8;
-        bottom_padding = 8;
-        left_padding = 8;
-        right_padding = 8;
-        window_gap = 8;
-      };
-      extraConfig = ''
-        yabai -m rule --add app="1Password" manage=off
-        yabai -m rule --add app="Bitwarden" manage=off
-        yabai -m rule --add app="Calendar" manage=off
-        yabai -m rule --add app="Dash" manage=off
-        yabai -m rule --add app="Discord" manage=off
-        yabai -m rule --add app="Docker" manage=off
-        yabai -m rule --add app="Docker\ Desktop" manage=off
-        yabai -m rule --add app="Finder" manage=off
-        yabai -m rule --add app="Insomnia" manage=off
-        yabai -m rule --add app="Licecap" manage=off
-        yabai -m rule --add app="Maps" manage=off
-        yabai -m rule --add app="Notes" manage=off
-        yabai -m rule --add app="Notion" manage=off
-        yabai -m rule --add app="Parallels\ Desktop" manage=off
-        yabai -m rule --add app="Photo\ Booth" manage=off
-        yabai -m rule --add app="Podcasts" manage=off
-        yabai -m rule --add app="Postman" manage=off
-        yabai -m rule --add app="Preview" manage=off
-        yabai -m rule --add app="QuickTime" manage=off
-        yabai -m rule --add app="RapidAPI" manage=off
-        yabai -m rule --add app="Stocks" manage=off
-        yabai -m rule --add app="System" manage=off
-        yabai -m rule --add app="Todoist" manage=off
-        yabai -m rule --add app="Transmission" manage=off
-        yabai -m rule --add app="Weather" manage=off
-      '';
-    };
-    skhd = {
-      enable = false;
-      skhdConfig = ''
-        alt + cmd - h : yabai -m window --warp west
-        alt + cmd - l : yabai -m window --warp east
-        alt + cmd - k : yabai -m window --warp north
-        alt + cmd - j : yabai -m window --warp south
-        # maximize a window
-        alt + cmd - f : yabai -m window --toggle zoom-fullscreen
-        # balance out tree of windows (resize to occupy equal space)
-        alt + cmd - b : yabai -m space --balance
-        # rotate layout
-        alt + cmd - r : yabai -m space --rotate 90
-        # move windows prev/next space
-        alt + cmd - left : yabai -m window --space prev; yabai -m window --focus west
-        alt + cmd - right : yabai -m window --space next; yabai -m display --focus east
-        # float / unfloat window and center on screen
-        alt + cmd - t : yabai -m window --toggle float;\
-        yabai -m window --grid 4:4:1:1:2:2
-      '';
-    };
     lorri = {
       enable = true;
     };
@@ -133,8 +56,8 @@
       "postgresql@15"
       "git-crypt"
       "gpg"
-      "vips"
-      "python-setuptools" # awsume deps
+      "vips" # Image processing library
+      # "python-setuptools" # awsume deps
     ];
     casks = [
       "dash"
