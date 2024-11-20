f236a3358c9c54f8d1ab4d90e2e72f776d557f6f
Alban Diguer <alban.diguer@pretto.fr>
2024-11-20 14:14:36 +0100

    chore: +colorschemes

--- home/programs/neovim/nvim/lua/custom/plugins/colorschemes.lua
+++ home/programs/neovim/nvim/lua/custom/plugins/colorschemes.lua
@@ -1,11 +1,12 @@
 return {
   'AlexvZyl/nordic.nvim',
   'catppuccin/nvim',
+  'olivercederborg/poimandres.nvim',
+  'rebelot/kanagawa.nvim',
   'sainnhe/edge',
   'sainnhe/everforest',
   'sainnhe/gruvbox-material',
   'sainnhe/sonokai',
   'savq/melange-nvim',
   'slugbyte/lackluster.nvim',
-  'olivercederborg/poimandres.nvim',
 }
