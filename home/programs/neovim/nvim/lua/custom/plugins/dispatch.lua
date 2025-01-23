3e527235081a521bf7823485df04eabcbdb7c705
Alban Diguer <alban.diguer@gmail.com>
2025-01-23 23:40:01 +0100

    fix: vim-dispatch not lazy

--- home/programs/neovim/nvim/lua/custom/plugins/dispatch.lua
+++ home/programs/neovim/nvim/lua/custom/plugins/dispatch.lua
@@ -1,9 +1,10 @@
 return {
-	'tpope/vim-dispatch',
-	keys = {
-		{ ',d', ':Dispatch', { desc = 'Dispatch (default make)' } }
-	},
-	config = function()
-		vim.g.dispatch_tmux_height = '35% -h'
-	end,
+  'tpope/vim-dispatch',
+  lazy = false,
+  keys = {
+    { ',d', ':Dispatch', { desc = 'Dispatch (default make)' } },
+  },
+  config = function()
+    vim.g.dispatch_tmux_height = '35% -h'
+  end,
 }
