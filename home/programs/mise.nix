f8e8e23e52db42666554a85b098110fd678316e6
Alban Diguer <alban.diguer@gmail.com>
2025-02-01 23:24:29 +0100

    feat: +uv

--- home/programs/mise.nix
+++ home/programs/mise.nix
@@ -9,12 +9,13 @@
     # see this for dotfiles https://mise.jdx.dev/lang/ruby.html#default-gems
     globalConfig = {
       tools = {
-        node = "latest";
-        gcloud = "latest";
-        ruby = "latest";
-        terraform = "latest";
         # postgres = "14.15";
         aws-cli = "latest";
+        gcloud = "latest";
+        node = "latest";
+        ruby = "latest";
+        terraform = "latest";
+        uv = "latest";
       };
     };
   };
