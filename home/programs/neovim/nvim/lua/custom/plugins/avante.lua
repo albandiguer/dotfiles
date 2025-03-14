99786e1e0ef5b32707f8ab1b7b8bb24cc06df9c9
Alban Diguer <alban.diguer@gmail.com>
2025-03-14 22:09:23 +0100

    config: avante models

--- home/programs/neovim/nvim/lua/custom/plugins/avante.lua
+++ home/programs/neovim/nvim/lua/custom/plugins/avante.lua
@@ -19,6 +19,16 @@ return {
         __inherited_from = 'copilot',
         model = 'claude-3.7-sonnet',
       },
+      copilot_claude_thought = {
+        __inherited_from = 'copilot',
+        model = 'claude-3.7-sonnet-thought',
+        temperature = 1,
+        max_tokens = 20000,
+      },
+      copilot_openai = {
+        __inherited_from = 'copilot',
+        model = 'gemini-2.0-flash-001',
+      },
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
