9664524eab72ef45377587526f10815a01b38234
Alban Diguer <alban.diguer@gmail.com>
2025-03-14 21:59:26 +0100

    fix: avante config

--- home/programs/neovim/nvim/lua/custom/plugins/avante.lua
+++ home/programs/neovim/nvim/lua/custom/plugins/avante.lua
@@ -25,11 +25,15 @@ return {
   build = 'make',
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
+    'nvim-treesitter/nvim-treesitter',
     'stevearc/dressing.nvim',
     'nvim-lua/plenary.nvim',
     'MunifTanjim/nui.nvim',
     --- The below dependencies are optional,
-    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+    'echasnovski/mini.pick', -- for file_selector provider mini.pick
+    'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+    'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+    'ibhagwan/fzf-lua', -- for file_selector provider fzfim-web-devicons', -- or echasnovski/mini.icons
     'zbirenbaum/copilot.lua', -- for providers='copilot'
     {
       -- support for image pasting
