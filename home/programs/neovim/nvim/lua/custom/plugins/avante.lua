4b07dc15a8b68d53e7608879895f3124b0226ba4
Alban Diguer <alban.diguer@gmail.com>
2025-04-13 01:06:54 +0200

    chore: up deps

--- home/programs/neovim/nvim/lua/custom/plugins/avante.lua
+++ home/programs/neovim/nvim/lua/custom/plugins/avante.lua
@@ -75,8 +75,12 @@ return {
       },
     },
     {
-      -- Make sure to set this up properly if you have lazy=true
       'MeanderingProgrammer/render-markdown.nvim',
+      dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+      -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+      -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+      ---@module 'render-markdown'
+      ---@type render.md.UserConfig
       opts = {
         file_types = { 'markdown', 'Avante' },
       },
