3cb1f6742d971cdc5a0850861556506b7ad5db13
Alban Diguer <alban.diguer@gmail.com>
2025-06-11 10:43:33 +0200

    chore: separate file for copilot.lua

---
+++ home/programs/neovim/nvim/lua/custom/plugins/copilot.lua
@@ -0,0 +1,13 @@
+return { -- Copilot
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  opts = {
+    suggestion = { enabled = false },
+    panel = { enabled = false },
+    filetypes = {
+      markdown = true,
+      help = true,
+    },
+  },
+}
