f305b0c7edf660347b47629ea292a6dfdb1a51d3
Alban Diguer <alban.diguer@gmail.com>
2025-07-06 21:25:11 +0200

    feat: +atuin

---
+++ home/programs/atuin.nix
@@ -0,0 +1,43 @@
+{pkgs, ...}: {
+  programs.atuin = {
+    enable = true;
+    enableFishIntegration = true;
+    
+    flags = [
+      "--disable-up-arrow"
+    ];
+    
+    settings = {
+      auto_sync = true;
+      sync_frequency = "5m";
+      sync_address = "https://api.atuin.sh";
+      
+      search_mode = "fuzzy";
+      filter_mode = "global";
+      filter_mode_shell_up_key_binding = "session";
+      
+      style = "compact";
+      show_preview = true;
+      show_help = true;
+      exit_mode = "return-original";
+      
+      history_filter = [
+        "^ls"
+        "^cd"
+        "^pwd"
+        "^exit"
+        "^clear"
+      ];
+      
+      secrets_filter = true;
+      
+      enter_accept = true;
+      
+      keymap_mode = "auto";
+      keymap_cursor = {
+        vim_insert = "steady-bar";
+        vim_normal = "steady-block";
+      };
+    };
+  };
+}
