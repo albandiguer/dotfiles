b63314f2e24093eee91fbe31c45e87fbc7c26f51
Alban Diguer <alban.diguer@pretto.fr>
2025-07-30 09:57:52 +0200

    chore: hide bashls alerts on ununsed env vars

--- home/programs/neovim/nvim/init.lua
+++ home/programs/neovim/nvim/init.lua
@@ -758,6 +758,19 @@ require('lazy').setup({
             },
           },
         },
+
+        -- disable checks for .env https://github.com/LazyVim/LazyVim/discussions/4027#discussioncomment-10425524
+        -- FIXME:
+        bashls = {
+          handlers = {
+            ['textDocument/publishDiagnostics'] = function(err, res, ...)
+              local file_name = vim.fn.fnamemodify(vim.uri_to_fname(res.uri), ':t')
+              if string.match(file_name, '^%.env') == nil then
+                return vim.lsp.diagnostic.on_publish_diagnostics(err, res, ...)
+              end
+            end,
+          },
+        },
       }
 
       -- Ensure the servers and tools above are installed
