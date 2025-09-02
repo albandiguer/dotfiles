25e4f125d77dad5d3faa4eeeab58be558a36521e
Alban Diguer <alban.diguer@pretto.fr>
2025-09-02 10:11:11 +0200

    chore: obsidian config

--- home/programs/neovim/nvim/lua/custom/plugins/obsidian.lua
+++ home/programs/neovim/nvim/lua/custom/plugins/obsidian.lua
@@ -45,7 +45,28 @@ return {
       -- Where to put new notes. Valid options are
       -- _ "current_dir" - put new notes in same directory as the current buffer.
       -- _ "notes_subdir" - put new notes in the default notes subdirectory.
-      new_notes_location = 'Fleeting/',
+      new_notes_location = 'notes_subdir',
+
+      -- Optional, customize how note IDs are generated given an optional title.
+      ---@param title string|?
+      ---@return string
+      note_id_func = function(title)
+        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+        -- In this case a note with the title 'My new note' will be given an ID that looks
+        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'.
+        -- You may have as many periods in the note ID as you'd like—the ".md" will be added automatically
+        local suffix = ''
+        if title ~= nil then
+          -- If title is given, transform it into valid file name.
+          suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+        else
+          -- If title is nil, just add 4 random uppercase letters to the suffix.
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
+        end
+        return tostring(os.time()) .. '-' .. suffix
+      end,
 
       templates = {
         folder = '_assets/templates',
