b37ea287831f8642cf3570ab3ffc25d0e574d17c
Alban Diguer <alban.diguer@gmail.com>
2025-11-16 22:28:28 +0100

    feat: +cutlass nvim

---
+++ home/programs/neovim/nvim/lua/custom/plugins/cutlass.lua
@@ -0,0 +1,8 @@
+return {
+  'gbprod/cutlass.nvim',
+  opts = {
+    -- your configuration comes here
+    -- or don't set opts to use the default settings
+    -- refer to the configuration section below
+  },
+}
