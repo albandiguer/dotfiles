6017c4b6a519c528726bcdc999a6f31bd77a8dcf
Alban Diguer <alban.diguer@pretto.fr>
2025-11-18 22:39:07 +0100

    Revert "feat: +cutlass nvim"
    This reverts commit b37ea287831f8642cf3570ab3ffc25d0e574d17c.

--- home/programs/neovim/nvim/lua/custom/plugins/cutlass.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'gbprod/cutlass.nvim',
-  opts = {
-    -- your configuration comes here
-    -- or don't set opts to use the default settings
-    -- refer to the configuration section below
-  },
-}
