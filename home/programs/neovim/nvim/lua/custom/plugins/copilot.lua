5261a1c3060827144f520bf155bbce512c410996
Alban Diguer <alban.diguer@pretto.fr>
2026-01-06 20:35:35 +0100

    fix: Copilot.lua node command to always be latest node

--- home/programs/neovim/nvim/lua/custom/plugins/copilot.lua
+++ home/programs/neovim/nvim/lua/custom/plugins/copilot.lua
@@ -3,6 +3,7 @@ return { -- Copilot
   cmd = 'Copilot',
   event = 'InsertEnter',
   opts = {
+    copilot_node_command = vim.fn.expand('~/.local/share/mise/installs/node/latest/bin/node'),
     suggestion = { enabled = false },
     panel = { enabled = false },
     filetypes = {
