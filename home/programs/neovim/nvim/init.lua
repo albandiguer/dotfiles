529573ac98ef173bfc07d7da7637cabaf4e6000f
Alban Diguer <alban.diguer@gmail.com>
2026-02-19 23:11:49 +0100

    fix: ts config

--- home/programs/neovim/nvim/init.lua
+++ home/programs/neovim/nvim/init.lua
@@ -1122,60 +1122,61 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = {
-        'apex',
-        'bash',
-        'bibtex',
-        'cmake',
-        'dockerfile',
-        'editorconfig',
-        'fish',
-        'gitcommit',
-        'gitignore',
-        'graphql',
-        'haskell',
-        'hcl',
-        'html',
-        'javascript',
-        'jq',
-        'json',
-        'lua',
-        'mermaid',
-        'nix',
-        'python',
-        'query',
-        'ruby',
-        'sql',
-        'terraform',
-        'typescript',
-        'vim',
-        'vimdoc',
-        'yaml',
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<C-Space>',
-          node_incremental = '<C-Space>',
-          scope_incremental = false,
-          node_decremental = '<bs>',
+    config = function()
+      require('nvim-treesitter').setup {
+        ensure_installed = {
+          'apex',
+          'bash',
+          'bibtex',
+          'cmake',
+          'dockerfile',
+          'editorconfig',
+          'fish',
+          'gitcommit',
+          'gitignore',
+          'graphql',
+          'haskell',
+          'hcl',
+          'html',
+          'javascript',
+          'jq',
+          'json',
+          'lua',
+          'mermaid',
+          'nix',
+          'python',
+          'query',
+          'ruby',
+          'sql',
+          'terraform',
+          'typescript',
+          'vim',
+          'vimdoc',
+          'yaml',
         },
-      },
-      textobjects = { enable = true },
-    },
+        -- Autoinstall languages that are not installed
+        auto_install = true,
+        highlight = {
+          enable = true,
+          -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+          --  If you are experiencing weird indenting issues, add the language to
+          --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+          additional_vim_regex_highlighting = { 'ruby' },
+        },
+        indent = { enable = true, disable = { 'ruby' } },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<C-Space>',
+            node_incremental = '<C-Space>',
+            scope_incremental = false,
+            node_decremental = '<bs>',
+          },
+        },
+        textobjects = { enable = true },
+      }
+    end,
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
