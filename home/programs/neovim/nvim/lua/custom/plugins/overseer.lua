500bf2b1286ced1bb5e2e61e2649a1edcb0e343b
Alban Diguer <alban.diguer@pretto.fr>
2026-03-01 23:23:55 +0100

    chore: overseer config

--- home/programs/neovim/nvim/lua/custom/plugins/overseer.lua
+++ home/programs/neovim/nvim/lua/custom/plugins/overseer.lua
@@ -3,6 +3,29 @@ return {
   keys = {
     { '<leader>kr', '<cmd>OverseerRun<cr>', desc = 'Tas[k] Run' },
     { '<leader>kk', '<cmd>OverseerToggle<cr>', desc = 'Tas[k] Toggle' },
+    {
+      '<leader>kt',
+      function()
+        if vim.bo.filetype ~= 'ruby' then
+          return
+        end
+        local file = vim.fn.expand '%'
+        local line = vim.fn.line '.'
+        require('overseer').new_task({ cmd = { 'bin/rspec', file .. ':' .. line } }):start()
+      end,
+      desc = 'Tas[k] rspec at cursor',
+    },
+    {
+      '<leader>kT',
+      function()
+        if vim.bo.filetype ~= 'ruby' then
+          return
+        end
+        local file = vim.fn.expand '%'
+        require('overseer').new_task({ cmd = { 'bin/rspec', file } }):start()
+      end,
+      desc = 'Tas[k] rspec file',
+    },
   },
   config = function()
     require('overseer').setup()
