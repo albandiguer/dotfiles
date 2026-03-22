96dfa42fe2d7abfda7ade3f07768e9f1e7f1da68
Alban Diguer <alban.diguer@gmail.com>
2026-03-23 00:47:31 +0100

    feat: review mappings

--- home/programs/neovim/nvim/lua/custom/plugins/octo.lua
+++ home/programs/neovim/nvim/lua/custom/plugins/octo.lua
@@ -7,6 +7,12 @@ return {
   },
   keys = {
     { '<leader>O', '<CMD>Octo<CR>', desc = 'Octo' },
+    { '<leader>op', '<CMD>Octo pr list<CR>', desc = 'List PRs' },
+    { '<leader>oi', '<CMD>Octo issue list<CR>', desc = 'List Issues' },
+    { '<leader>or', '<CMD>Octo review start<CR>', desc = 'Start PR Review' },
+    { '<leader>oc', '<CMD>Octo comment add<CR>', desc = 'Add Comment' },
+    { '<leader>oC', '<CMD>Octo review comments<CR>', desc = 'Review Comments' },
+    { '<leader>os', '<CMD>Octo review submit<CR>', desc = 'Submit Review' },
   },
   dependencies = {
     'nvim-lua/plenary.nvim',
