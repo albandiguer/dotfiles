7cbf5bf16440450d783b5fbc7df4e0b25e4115f8
Alban Diguer <alban.diguer@gmail.com>
2026-03-22 16:52:20 +0100

    feat: oc +context7

--- home/programs/opencode.nix
+++ home/programs/opencode.nix
@@ -36,6 +36,11 @@
           ];
           enabled = true;
         };
+        context7 = {
+          type = "remote";
+          url = "https://mcp.context7.com/mcp";
+          enabled = true;
+        };
       };
     };
 
