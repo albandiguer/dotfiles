af346860981ce471ae296ac0057da15b57b66817
Alban Diguer <alban.diguer@pretto.fr>
2026-03-25 22:17:14 +0100

    minor: cleanup

--- home/programs/neovim/nvim/init.lua
+++ home/programs/neovim/nvim/init.lua
@@ -477,9 +477,6 @@ require('lazy').setup({
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<leader>a', builtin.oldfiles, { desc = '[A] Old files' })
-      vim.keymap.set('n', '<leader>l', builtin.oldfiles, { desc = '[L]azygit' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sf', function()
         builtin.find_files { hidden = true }
