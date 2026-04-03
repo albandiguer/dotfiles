a81238e72f63fb2953c58680f82fa812353e5273
Alban Diguer <alban.diguer@gmail.com>
2026-04-03 22:01:21 +0200

    chore: markview checkboxes styling

--- home/programs/neovim/nvim/lua/custom/plugins/markview.lua
+++ home/programs/neovim/nvim/lua/custom/plugins/markview.lua
@@ -7,6 +7,28 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    require('markview').setup()
+    require('markview').setup {
+      markdown_inline = {
+        enable = true,
+        checkboxes = {
+          enable = true,
+          checked = { text = '☑', hl = 'MarkviewCheckboxChecked' },
+          unchecked = { text = '☐', hl = 'MarkviewCheckboxUnchecked' },
+        },
+        block_references = { enable = true, default = {} },
+        emails = { enable = true, default = {} },
+        embed_files = { enable = true, default = {} },
+        emoji_shorthands = { enable = true },
+        entities = { enable = true },
+        escapes = { enable = true },
+        footnotes = { enable = true, default = {} },
+        highlights = { enable = true, default = {} },
+        hyperlinks = { enable = true, default = {} },
+        images = { enable = true, default = {} },
+        inline_codes = { enable = true },
+        internal_links = { enable = true, default = {} },
+        uri_autolinks = { enable = true, default = {} },
+      },
+    }
   end,
 }
