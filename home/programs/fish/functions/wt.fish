5dda05670987ff9165f6c92ee2a66d24125a98b2
Alban Diguer <alban.diguer@pretto.fr>
2026-04-08 22:18:18 +0200

    chore: better handling of wt in monorepos

--- home/programs/fish/functions/wt.fish
+++ home/programs/fish/functions/wt.fish
@@ -1,9 +1,25 @@
 if test (count $argv) -ge 2; and test $argv[1] = "create"
+    # Build worktree path: parent/repo-name.subpath.branch
+    set -l repo_root (git rev-parse --show-toplevel 2>/dev/null)
+    set -l repo_name (basename $repo_root)
+    set -l parent_dir (dirname $repo_root)
+    set -l current (pwd)
+    set -l branch $argv[2]
+    set -l worktree_path
+
+    if test "$current" = "$repo_root"
+        set worktree_path "$parent_dir/$repo_name.$branch"
+    else
+        set -l rel_path (string replace "$repo_root/" "" "$current")
+        set -l rel_sanitized (string replace -ra '/' '-' $rel_path)
+        set worktree_path "$parent_dir/$repo_name-$rel_sanitized.$branch"
+    end
+
     # Get worktrees before creation
     set -l before_worktrees (git worktree list --porcelain 2>/dev/null | grep "^worktree" | string replace "worktree " "")
 
-    # Create the worktree
-    command wt switch -c $argv[2] --no-cd
+    # Create the worktree at the computed path
+    WORKTRUNK_WORKTREE_PATH=$worktree_path command wt switch -c $argv[2] --no-cd
     set -l wt_status $status
 
     # If successful, add new worktree to zoxide
